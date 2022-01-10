bcddf53d8a728e81228085d5ee7ac59de98f55da
DESKTOP-ED9A3K2\Alexis O. Perez <alexisperezglez@gmail.com>
2022-01-10 07:31:55 -0500

    form

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import { useState } from "react"
 import indexStyles from '../styles/index.module.css';
-import { Button, TextField, Input } from '@mui/material';
+import { Button, Checkbox } from '@mui/material';
 
 const Home = () => {
   const formDataInit = {
-    cert: undefined,
-    document: undefined,
-    password: ''
+    motivo: false,
+    ubicacion: false,
+    estadoRevocacion: false,
+    nombre: false,
+    fecha: false,
+    logotipo: false,
+    img: undefined
   }
 
   const [formData, setFormData] = useState(formDataInit)
@@ -18,7 +22,7 @@ const Home = () => {
     console.log(e)
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value || e.target.files[0]
+      [e.target.name]: e.target.value
     })
     console.log('form data', formData)
   }
@@ -33,33 +37,30 @@ const Home = () => {
   const { document, password, cert } = formData
   return (
     <form onSubmit={(event) => printText(event)} className={indexStyles.sign_form}>
-      <div className={indexStyles.form_controls}>
-        <label>Documento</label>
-        {/* <input type="file" name="document" onChange={(e) => handleFormChange(e)} value={document} accept=".pdf"/> */}
-        <Button
+      {/* <label>Creacion y aspecto</label>
+      <Checkbox onChange={(e) => handleFormChange(e)} name="motivos" label="Mostrar motivos" />
+      <Checkbox onChange={(e) => handleFormChange(e)} name="ubicacion" label="Mostrar ubicacion e informacion de contacto" />
+      <Checkbox onChange={(e) => handleFormChange(e)} name="estadoRevocacion" label="Incluir estado de revocacion de la firma" /> */}
+
+      {/* <label>Validacion de la firma</label>
+      <label>Controlar como y cuando se validan las firmas</label>
+      <Checkbox onChange={(e) => handleFormChange(e)} name="" label="Verificar las firmas al abrir el documento" />
+      <Checkbox onChange={(e) => handleFormChange(e)} name="" label="Confiar en todos los certificados del repositorio" />
+      <Checkbox onChange={(e) => handleFormChange(e)} name="" label="Confiar en todos los certificados raiz de windows" /> */}
+
+      <label>Configurar texto</label>
+      <Checkbox onChange={(e) => handleFormChange(e)} name="nombre" label="Nombre" />
+      <Checkbox onChange={(e) => handleFormChange(e)} name="fecha" label="Fecha" />
+      <Checkbox onChange={(e) => handleFormChange(e)} name="ubicacion" label="Ubicacion" />
+      <Checkbox onChange={(e) => handleFormChange(e)} name="motivo" label="Motivo" />
+      <Checkbox onChange={(e) => handleFormChange(e)} name="logotipo" label="Logotipo" />
+      <Checkbox onChange={(e) => handleFormChange(e)} name="etiqueta" label="Etiquetas" />
+
+      <Button
           variant="contained"
           component="label"
         >
-          {!!document ? document.name : 'Upload Doc'}
-          <input
-            type="file"
-            name="document" 
-            onChange={(e) => handleFormChange(e)} 
-            value={document} 
-            accept=".pdf"
-            hidden
-          />
-        </Button>
-        <small>Documeto .pdf que desea firmar</small>
-      </div>
-      <div className={indexStyles.form_controls}>
-        <label>Certificado</label>
-        {/* <input type="file" name="cert" onChange={(e) => handleFormChange(e)} value={cert} accept=".p12"/> */}
-        <Button
-          variant="contained"
-          component="label"
-        >
-          Upload Cert
+          Upload Image
           <input
             type="file"
             name="cert" 
@@ -68,23 +69,7 @@ const Home = () => {
             accept=".p12"
             hidden
           />
-        </Button>
-        {/* <Input type="file" name="cert" onChange={(e) => handleFormChange(e)} value={cert} accept=".p12"/> */}
-        <small>Llave publica para firmas digitales .p12</small>
-      </div>
-      <div className={indexStyles.form_controls}>
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          name="password"
-          variant="filled"
-          value={password}
-          onChange={(e) => handleFormChange(e)}
-        />
-        <small>Contrasenna de su certificado digital para realizar la firma</small>
-      </div>
+
       <Button variant="contained" onClick={(e) => validateAndSendDataForm(e)}>Contained</Button>
     </form>
   )
